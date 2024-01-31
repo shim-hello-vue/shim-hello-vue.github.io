d91f9a106dddb6315034dd9d515933cf4d85b625
shimguh <shimguh@naver.com>
2024-01-31 17:00:21 +0900

    if

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,12 @@ const categories = ref(category)
 
 <template>
 <div v-for="item in categories">
+<div v-if="item.rank < 11">
 	<br> <div> rank : {{item.rank}} </div>
 	<div> name : {{ item.name }} </div>
 	<div> artist : {{ item.artist }} </div>
 	<div> peak rank : {{item.peak_rank}} </div>
 	<div> weeks on chart : {{item.weeks_on_chart}} </div> <br> <hr>
+</div>
 </div>
 </template>

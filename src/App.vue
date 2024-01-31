ddd075da98330b65a84e78b7ce330ae67806c6c6
shimguh <shimguh@naver.com>
2024-01-31 11:39:02 +0900

    hello count + rev name

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+
+const count = ref({
+	cnt: 0,
+	name: "shim"
+})
+
+function increment() {
+	count.value.cnt++
+	count.value.name = count.value.name.split('').reverse().join('')
+}
+
+onMounted(() => {
+	console.log(`The initial count is ${count.value}.`)
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<button @click="increment">Count is : {{count.cnt}} {{count.name}}</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+	font-weight: bold;
 }
 </style>

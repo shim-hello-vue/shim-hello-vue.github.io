d4dc0b700d6dc891182f492b3da409fcb720d27e
shimguh <shimguh@naver.com>
2024-02-01 16:41:45 +0900

    components2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import {ref} from 'vue'
 import Child from './components/Child.vue'
+
+const pocketMoney = ref('부모 컴포넌트로부터 <✉️>을 전달받았어요!')
 </script>
 
 <template>
-<Child />
+<Child :pocketMoney="pocketMoney"/>
 </template>

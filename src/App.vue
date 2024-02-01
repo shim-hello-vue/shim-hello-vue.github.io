ccff2ee65aeb23cf59c798ded2ea8d0316b2d61d
shimguh <shimguh@naver.com>
2024-02-01 16:09:31 +0900

    todo

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,19 @@ const newTodo = ref('')
 const todos = ref([
 	{id: id++, issue: 'Backlog'}, {id: id++, issue: 'Todo'}, {id : id++, issue: 'Going Hawaii'}
 ])
+
+function addTodo() {
+	todos.value.push({id: id++, issue: newTodo.value})
+}
+
+function removeTodo(todo) {
+todos.value = todos.value.filter((t) => t !== todo)
+}
 </script>
 
 <template>
-<form @submit="addTodo">
+
+<form @submit.prevent="addTodo">
 <input v-model="newTodo">
 <button>할 일 추가</button>
 </form>
